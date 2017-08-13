995cf706c3eb4ffc987b9c97a3bad7378c23d91a
Roy Mosby <egomadking@users.noreply.github.com>
2017-08-12 22:38:03 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_Ep9pCtsdoIiavyGkzPNJ4Vb7JwsU744l8Yax1_Az9k/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Ztk8S8i_WVra1AGeyA6wWH4a6JOmCO00UAhdae6t7Qk/edit#gid=164271551';
